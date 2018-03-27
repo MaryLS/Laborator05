ac8f329cb81d42c7d9f0b75d74784bceb1e3a2e5
Student User <student@cti.pub.ro>
2018-03-27 19:42:39 +0300

    pana la 8 inclusiv

--- labtasks/StartedService/StartedServiceActivity/app/src/main/java/ro/pub/cs/systems/eim/lab05/startedserviceactivity/view/StartedServiceBroadcastReceiver.java
+++ labtasks/StartedService/StartedServiceActivity/app/src/main/java/ro/pub/cs/systems/eim/lab05/startedserviceactivity/view/StartedServiceBroadcastReceiver.java
@@ -1,4 +1,5 @@
 package ro.pub.cs.systems.eim.lab05.startedserviceactivity.view;
+import ro.pub.cs.systems.eim.lab05.startedserviceactivity.general.Constants;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -19,6 +20,19 @@ public class StartedServiceBroadcastReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         // TODO: exercise 7 - get the action and the extra information from the intent
         // and set the text on the messageTextView
+        String action = intent.getAction();
+        String data = null;
+        switch (action) {
+            case Constants.ACTION_STRING:
+                data = intent.getStringExtra(Constants.DATA);
+            case Constants.ACTION_INTEGER:
+                data = String.valueOf(intent.getIntExtra(Constants.DATA, -1));
+            case Constants.ACTION_ARRAY_LIST:
+                data = intent.getStringExtra(Constants.DATA).toString();
+        }
+
+        if (messageTextView != null)
+            messageTextView.setText(messageTextView.getText().toString() + "\n" + data);
 
         // TODO: exercise 9 - restart the activity through an intent
         // if the messageTextView is not available
